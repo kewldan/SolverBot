e81ed2c10887763add6e5eb2362355454a3d047d
kewldan <kewldanil1@gmail.com>
2023-10-01 21:28:50 +0300

    sh script

---
+++ rebuild.sh
@@ -0,0 +1,5 @@
+docker container stop sb
+docker container rm sb
+docker image rm sbi
+docker build -t sbi .
+docker run -d --name sb --mount type=bind,source=/home/ylous/SolverBot/data,target=/Application/data sbi
