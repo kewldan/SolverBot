5bfbea1297b3dfd5847d2f74eee9032d62bb3015
kewldan <kewldanil1@gmail.com>
2024-02-06 19:54:49 +0300

    fix

--- src/dynamic/handlers/callback/users.py
+++ src/dynamic/handlers/callback/users.py
@@ -18,8 +18,7 @@ async def on_distribute_callback(query: CallbackQuery):
         user = User(**user)
         users_response.append(
             f'<b>{len(users_response) + 1}.</b> {html.escape(("@" + user.username) if user.username else "")} [<code>{user.id}</code>] - '
-            f'<code>{user.solved}</code> | {get_timestamp(user.joined)}'
-            f'{html.escape((" через " + user.referral) if user.referral else "")}')
+            f'<code>{user.solved}</code>')
 
     response = "\n".join(users_response)
     await query.message.edit_text(f'<b>Пользователи ({len(users_response)}):</b>\n\n{response}')
