5db8d6e3a379e96b07a6ead7a3eb3e73406b45f5
kewldan <kewldanil1@gmail.com>
2024-02-10 13:00:29 +0300

    fix2

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,15 @@
 from kwldn_bot import XBot
+from kwldn_bot.modules.error_handler import add_to_router
 
 import api
 from handlers.buttons import buttons_router
 from handlers.callback import callbacks_router
 from handlers.commands import commands_router
 from middlewares.user_fetch import UserFetchMiddleware
-from kwldn_bot.modules.error_handler import add_to_router
 
 bot = XBot(api.config.bot.token)
 
-add_to_router(bot.router, 'https://t.me/kwld_manager')
+add_to_router(bot.router, api.config.bot.owners, 'https://t.me/kwld_manager')
 bot.router.message.middleware(UserFetchMiddleware())
 bot.router.callback_query.middleware(UserFetchMiddleware())
 
