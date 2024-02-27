a58a495ff199bac539a678f6db78dd41a22d531c
kewldan <kewldanil1@gmail.com>
2024-02-27 11:28:13 +0000

    fixes

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 docker container stop sb
 docker container rm sb
 docker image rm sbi
-docker build -t sbi .
+docker build --no-cache -t sbi .
 docker run -d --name sb --restart unless-stopped --mount type=bind,source="$PWD"/data,target=/usr/app/data sbi
