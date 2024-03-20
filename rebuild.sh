da3a921c31762a89d7c5d07ec6977ce4e9cc96fe
lolkof <lolkofprog@gmail.com>
2024-03-21 01:30:40 +0300

    rebuild fix

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@ docker container stop sb
 docker container rm sb
 docker image rm sbi
 docker build -t sbi .
-docker run --network=host --name  sb -d -p 3036:3036 --restart unless-stopped --mount type=bind,source="$PWD"/data,target=/usr/app/data sbi
+docker run --name  sb -d -p 3036:3036 --restart unless-stopped --mount type=bind,source="$PWD"/data,target=/usr/app/data sbi
