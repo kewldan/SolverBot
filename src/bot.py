ba391ca836b5460fe398f6da456e0b3b19f8bc45
kewldan <kewldanil1@gmail.com>
2024-05-15 14:46:41 +0300

    disable

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ from kwldn_bot.modules.error_handler import add_to_router
 
 from config import config
 from handlers import commands_router, callbacks_router, buttons_router
-from middlewares import PauseMiddleware, UserFetchMiddleware
+from middlewares import UserFetchMiddleware
 
 if config.bot.debug:
     bot = XBot(config.bot.token)
@@ -11,8 +11,8 @@ else:
     bot = XMultiBot(config.bot.token, config.web.base_url, config.web.port)
 
 add_to_router(bot.router, bot.main_bot, config.bot.owners, 'https://t.me/kwld_manager')
-bot.router.message.middleware(PauseMiddleware())
-bot.router.callback_query.middleware(PauseMiddleware())
+# bot.router.message.middleware(PauseMiddleware())
+# bot.router.callback_query.middleware(PauseMiddleware())
 bot.router.message.middleware(UserFetchMiddleware())
 bot.router.callback_query.middleware(UserFetchMiddleware())
 
