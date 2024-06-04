de8d6f2d629f5c66bd33c92e1a4c2fb5a1a40298
kewldan <kewldanil1@gmail.com>
2024-06-04 23:09:27 +0000

    traeffik

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@ docker container stop sb
 docker container rm sb
 docker image rm sbi
 docker build -t sbi .
-docker run --restart unless-stopped --name sb -d --env VIRTUAL_HOST=sb.kewldan.ru --restart unless-stopped --mount type=bind,source="$PWD"/data,target=/usr/app/data sbi
+docker run --restart unless-stopped --name sb -d -l "traefik.http.routers.solver-bot.rule=Host(\`sb.kewldan.ru\`)" -l traefik.enable=true -l traefik.http.services.solver-bot.loadbalancer.server.port=80 --net virtual-hosts --restart unless-stopped --mount type=bind,source="$PWD"/data,target=/usr/app/data sbi
